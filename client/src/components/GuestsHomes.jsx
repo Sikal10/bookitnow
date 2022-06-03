6999cbd4aabf7858eb992c90adbb0cde21bd3bbc
Sikal <sikal.sikal.ss@gmail.com>
2022-06-04 00:02:43 +0100

    fetch hotels from the server

--- client/src/components/GuestsHomes.jsx
+++ client/src/components/GuestsHomes.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import {guestsHomes} from "../properties";
+import {useQuery} from "react-query";
+import {getFeaturedHotels} from "../api/hotels.api";
 
 const GuestsHomes = () => {
+    const {data} = useQuery("featuredHotels", () => getFeaturedHotels());
+
     return (
         <div className={"guests__home"}>
-            {guestsHomes.map((item, index) => <div className={"guests__home-item"}>
-                    <img className={"guests__home-item-img"} src={item.img} alt=""/>
-                    <h2>{item.name}</h2>
-                    <p className={"guest-city"}>{item.city}</p>
-                    <p className={"guest-price"}>Starting from ${item.price}</p>
-                    <div className={"guest-rating"}>
-                        <span>{item.rating} </span>
-                        <p>Excellent</p>
-                    </div>
+            {data?.hotels?.map((item, index) => <div key={index} className={"guests__home-item"}>
+                <img className={"guests__home-item-img"} src={item.photos[0]} alt=""/>
+                <h2>{item.name}</h2>
+                <p className={"guest-city"}>{item.city}</p>
+                <p className={"guest-price"}>Starting from ${item.cheapestPrice}</p>
+                {item.rating && <div className={"guest-rating"}>
+                    <span>{item.rating} </span>
+                    <p>Excellent</p>
+                </div>}
 
             </div>)}
         </div>
--- client/src/pages/Hotels.jsx
+++ client/src/pages/Hotels.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import Navigation from "../components/Navigation";
 import Header from "../components/Header";
 import {useLocation} from "react-router-dom";
@@ -7,6 +7,8 @@ import DateRange from "react-date-range/dist/components/DateRange";
 import SearchResult from "../components/SearchResult";
 import Footer from "../components/Footer";
 import SaveTime from "../components/SaveTime";
+import {useQuery} from "react-query";
+import {getSearchedHotels} from "../api/hotels.api";
 
 const Hotels = () => {
     const location = useLocation();
@@ -15,11 +17,17 @@ const Hotels = () => {
     const [date, setDate] = useState(location.state.date);
     const [options, setOptions] = useState(location.state.options);
     const [isDateOpen, setIsDateOpen] = useState(false);
-
+    const [minPrice, setMinPrice] = useState(undefined);
+    const [maxPrice, setMaxPrice] = useState(undefined);
 
     const startDate = date[0].startDate;
     const endDate = date[0].endDate;
 
+    const {isLoading, data, refetch} = useQuery("searchedHotels", () => getSearchedHotels(destination, minPrice, maxPrice));
+
+    const handleSearch = async () => await refetch();
+
+    if (isLoading) return <h2>Loading...</h2>
 
     return (
         <div>
@@ -59,11 +67,11 @@ const Hotels = () => {
                                 <div className="option-item-container">
                                     <div className="search__option-item">
                                         <span className={"option-item-text"}>Min price (per night)</span>
-                                        <input type="number"/>
+                                        <input onChange={e => setMinPrice(e.target.value)} type="number"/>
                                     </div>
                                     <div className="search__option-item">
                                         <span className={"option-item-text"}>Max price (per night)</span>
-                                        <input type="number"/>
+                                        <input onChange={e => setMaxPrice(e.target.value)} type="number"/>
                                     </div>
                                     <div className="search__option-item">
                                         <span className={"option-item-text"}>Adult</span>
@@ -81,18 +89,11 @@ const Hotels = () => {
                             </div>
                         </div>
 
-                        <button className={"search-button"}>Search</button>
+                        <button onClick={handleSearch} className={"search-button"}>Search</button>
                     </div>
 
                     <div className="hotels__container-results">
-                        <SearchResult />
-                        <SearchResult />
-                        <SearchResult />
-                        <SearchResult />
-                        <SearchResult />
-                        <SearchResult />
-                        <SearchResult />
-                        <SearchResult />
+                        {data?.hotels?.map((hotel, index) => <SearchResult hotel={hotel} key={index} />)}
                     </div>
                 </div>
             </div>
